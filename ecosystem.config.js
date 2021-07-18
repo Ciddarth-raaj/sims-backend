abfa8574a325afe2586f9b3e246c7f50180c9f23
Ciddarth Raaj <ciddarthjeyakumar@gmail.com>
2021-07-18 19:34:28 +0530

    Changes port number

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,25 @@
 module.exports = {
-  apps: [{
-    script: "server.js",
-    env: {
-      NODE_ENV: "production",
+  apps: [
+    {
+      script: "server.js",
+      env: {
+        NODE_ENV: "production",
+        PORT: "8181",
+      },
     },
-  },],
+  ],
 
   deploy: {
     production: {
-      user: 'SSH_USERNAME',
-      host: 'SSH_HOSTMACHINE',
-      ref: 'origin/master',
-      repo: 'GIT_REPOSITORY',
-      path: 'DESTINATION_PATH',
-      'pre-deploy-local': '',
+      user: "SSH_USERNAME",
+      host: "SSH_HOSTMACHINE",
+      ref: "origin/master",
+      repo: "GIT_REPOSITORY",
+      path: "DESTINATION_PATH",
+      "pre-deploy-local": "",
       "post-deploy":
         "npm install && cd migrations/mysql && db-migrate up && cd ../../ && pm2 reload ecosystem.config.js --env production",
-      'pre-setup': ''
-    }
-  }
+      "pre-setup": "",
+    },
+  },
 };
